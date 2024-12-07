cd4fec4d3a9e3346f97a06f4252106c08859a19e
Ivan Saker <isaker@frba.utn.edu.ar>
2024-12-07 11:11:21 -0300

    Update solucion.pl

--- solucion.pl
+++ solucion.pl
@@ -1,15 +1,15 @@
-
-inmueble(persona(juan),casa(120),barrio(almagro)).
-inmueble(persona(fer),casa(110),barrio(flores)).
-
-
-
-inmueble(persona(nico),dpto(3,2),barrio(almagro)).
-inmueble(persona(alf),dpto(3,1),barrio(almagro)).
-inmueble(persona(vale),dpto(4,1),barrio(flores)).
+%1
+inmueble(persona(juan),casa(120),barrio(almagro),150000).
+inmueble(persona(fer),casa(110),barrio(flores),60000).
 
 
-inmueble(persona(julian),anio(2000),barrio(almagro)).
+
+inmueble(persona(nico),dpto(3,2),barrio(almagro),80000).
+inmueble(persona(alf),dpto(3,1),barrio(almagro),75000).
+inmueble(persona(vale),dpto(4,1),barrio(flores),95000).
+
+
+inmueble(persona(julian),anio(2000),barrio(almagro),140000).
 
 persona(felipe).
 persona(rocio).
@@ -17,26 +17,42 @@ persona(rocio).
 barrio(almagro).
 barrio(flores).
 
+%2
+esCopada(inmueble(_,dpto(Amb,Banio),_,_)):-
+Amb>3,
+Banio>1.
 
-propiedadCopada(Casa):-
-inmueble(_,espacio,_),
+esCopada(inmueble(_,casa(espacio),_,_)):-
 espacio>100.
 
-departamentoCopado(dep):-
-casa(_,Ambientes,Banios,_),
-Ambientes>3,
-Banios>1.
-
-loftCopado(loft):-
-casa(_,anio,_),
+esCopada(inmueble(_,loft(anio),_,_)):-
 anio>2015.
 
 barrioCopado(Barrio):-
 barrio(Barrio),
-forall((casa(_,_,Barrio)),propiedadCopada).
-%%falta
+forall(inmueble(_,Propiedad,Barrio,_), esCopada(inmueble(_,Propiedad,_,_))).
+
+%3
+
+esBarato(inmueble(_,dpto(Amb,_),_,_)):-
+Amb<3.
+
+esBarato(inmueble(_,casa(espacio),_,_)):-
+espacio<90.
+
+esBarato(inmueble(_,loft(anio),_,_)):-
+anio<2005.
+
+barrioCaro(Barrio):-
+    barrio(Barrio),
+    forall(inmueble(_,Propiedad,Barrio,_), not(esBarato(inmueble(_,Propiedad,_,_)))).
+
+%4
+
+comprarCasa(PlataDispo,)
 
 
-casaBarata(Casa):-
+
+
 
 
